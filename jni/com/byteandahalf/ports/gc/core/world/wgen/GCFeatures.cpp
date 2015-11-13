a317113665ffa4c9dd1a4f50f6bbcf03f3c472fe
byteandahalf <byteandahalf@gmail.com>
2015-11-13 11:34:16 -0500

    Added a (crashing) test custom ore.

--- jni/com/byteandahalf/ports/gc/core/world/wgen/GCFeatures.cpp
+++ jni/com/byteandahalf/ports/gc/core/world/wgen/GCFeatures.cpp
@@ -1,10 +1,12 @@
 #include "GCFeatures.h"
 #include "com/mojang/minecraftpe/world/level/biome/BiomeDecorator.h"
-#include "com/mojang/minecraftpe/world/level/levelgen/feature/Feature.h"
+#include "com/mojang/minecraftpe/world/level/levelgen/feature/OreFeature.h"
 #include "GCFeatureInfo.h"
 
 BiomeDecorator* GCFeatures::Decorator;
 std::vector<GCFeatureInfo> GCFeatures::features;
+
+std::unique_ptr<Feature> GCFeatures::testFeature;
 
 void GCFeatures::setDecorator(BiomeDecorator* decorator) {
 	Decorator = decorator;
@@ -12,12 +14,14 @@ void GCFeatures::setDecorator(BiomeDecorator* decorator) {
 
 void GCFeatures::initFeatures() {
 	//Init Feature unique_ptr here
+	testFeature.reset(new OreFeature(19, 0, 12));
 	
 	registerFeatures();
 }
 
 void GCFeatures::registerFeatures() {
 	//create FeatureInfo and push feature to global vector
+	features.push_back(GCFeatureInfo(GCFeatureInfo::GenType::SPAN, testFeature, 20, 0, 128));
 }
 
 void GCFeatures::populateFeatures(BlockSource* region, Random& random, const BlockPos& pos) {
--- jni/com/byteandahalf/ports/gc/core/world/wgen/GCFeatures.h
+++ jni/com/byteandahalf/ports/gc/core/world/wgen/GCFeatures.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <vector>
+#include <memory>
 class BlockSource;
 class BlockPos;
 class Random;
 class BiomeDecorator;
+class Feature;
 class GCFeatureInfo;
 
 class GCFeatures {
 	static BiomeDecorator* Decorator;
 	static std::vector<GCFeatureInfo> features;
+	
+	static std::unique_ptr<Feature> testFeature;
 	
 public:
 	static void setDecorator(BiomeDecorator*);
